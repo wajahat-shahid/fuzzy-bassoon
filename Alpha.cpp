c7622469b390b08e0754b959e73eebe6255b1fa4
Wajahat Shahid <71876606+wajahat-shahid@users.noreply.github.com>
2021-10-09 14:47:52 +0500

    Add files via upload

---
+++ Alpha.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+void IA()
+{
+	for(int i=0;i<10;i++)
+	cout<<"Peeno Mere Naal Weya Kr Ley"<<endl;
+	cout<<endl;
+}
+
+
+
+void Lub()
+{
+	for (int i=0;i<5;i++)
+		cout<<"Dirilis Ertugrul have been the most successfull cinematic work ever in the history."<<endl;
+	cout<<endl;
+}
+
+int main ()
+{
+	IA();
+	for (int i=0;i<5;i++)
+		cout<<"I love Pakistan."<<endl;	
+	
+	cout<<endl<<endl;
+	//Fahad Saab ki repository hai
+	
+	// Looping for 5 times
+	for (int i=0;i<5;i++)
+		cout<<"Ben Doctor."<<endl;	
+
+	bool flag;
+	flag= true;
+	if(flag)
+	cout<<"Bhai Nahana Parey gaa";
+	else
+	cout<<"Khaire hi hia Cheetey";
+}
